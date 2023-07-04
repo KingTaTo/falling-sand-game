545cf2345b2274f45750fc289906168e87468b77
KingTaTo <tangkhangvinh@gmail.com>
2023-07-03 20:58:27 -0400

    Initial commit

---
+++ DynamicArray.java
@@ -0,0 +1,237 @@
+// TO DO: add your implementation and JavaDocs.
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class DynamicArray<T> implements Iterable<T> {
+	private static final int INITCAP = 2; //default initial capacity
+	private T[] storage; //underlying array, you MUST use this for credit (do not change the name or type)
+	private int size = 0;
+
+	@SuppressWarnings("unchecked")
+	public DynamicArray(){
+		// constructor
+		// Initial capacity of the storage should be INITCAP
+		storage = (T[])new Object[INITCAP];
+	}
+
+	@SuppressWarnings("unchecked")
+	public DynamicArray(int initCapacity) {
+		//constructor
+		
+		// The initial capacity of the storage should be initCapacity
+		
+		// Throw IllegalArgumentException if initCapacity < 1
+		// Use this _exact_ error message for the exception
+		// (quotes are not part of the message):
+		//    "Capacity cannot be zero or negative."
+		if(initCapacity > 0)
+			storage = (T[])new Object[initCapacity];
+		else
+			throw new IllegalArgumentException("Capacity cannot be zero or negative.");
+	}
+
+	public int size() {	
+		// Report the current number of elements.
+		// O(1)
+		
+		return size;
+	}  
+		
+	public int capacity() {
+		// Report the max number of elements before expansion.
+		// O(1)
+		
+		return storage.length;
+	}
+	
+	public T set(int index, T value) {
+		// Change the item at the given index to be the given value.
+		// Return the old item at that index.
+		// Note: You cannot add new items with this method.
+		
+		// O(1)
+		
+		// For an invalid index, throw an IndexOutOfBoundsException
+		// Use this code to produce the correct error message for
+		// the exception (do not use a different message):
+		//	  "Index " + index + " out of bounds!"
+		if (index > size) {
+			throw	new IndexOutOfBoundsException("Index " + index + " out of bounds!");
+		}
+
+		T oldValue = storage[index];
+		storage[index] = value;
+		return oldValue;
+	}
+
+	public T get(int index) {
+		// Return the item at the given index
+		
+		// O(1)
+		
+		// Use the exception (and error message) described in set()
+		// for invalid indicies.
+		if (index > size()) {
+			throw	new IndexOutOfBoundsException("Index " + index + " out of bounds!");
+		}
+		return storage[index];
+	}
+
+	@SuppressWarnings("unchecked")
+	public boolean add(T value) {
+		// Append an element to the end of the list and return true.
+		storage[size] = value;
+		size++;
+		// Double the capacity if no space available.
+		if (size == capacity()) {
+			storage = Arrays.copyOf(storage, storage.length * 2);
+		}
+		// Amortized O(1)
+
+		return true;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void add(int index, T value) {
+		// Insert the given value at the given index. Shift elements if needed,  
+		// double capacity if no space available, throw an exception if you cannot
+		// insert at the given index. You _can_ append items with this method.
+		
+		// For the exception, use the same exception and message as set() and
+		// get()... however remember that the condition of the exception is
+		// different (different indexes are invalid).
+		
+		// O(N) where N is the number of elements currently in the list
+		if (index < 0 || index > capacity())
+			throw	new IndexOutOfBoundsException("Index " + index + " out of bounds!");
+
+		// Shifting elements to the right
+		for (int i = size; i > index; i--)
+			storage[i] = storage[i-1];
+		storage[index] = value;
+		size++;
+
+		// Expanding array if needed
+		if (size == capacity())
+			storage = Arrays.copyOf(storage, storage.length * 2);
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public T remove(int index) {
+		// Remove and return the element the given index. Shift elements
+		// to remove the gap. Throw an exception when there is an invalid
+		// index (see set(), get(), etc. above).
+
+		// Halve capacity of the storage if the number of elements falls
+		// below 1/3 of the capacity.
+		
+		// O(N) where N is the number of elements currently in the list
+
+		if (index < 0 || index > capacity())
+			throw	new IndexOutOfBoundsException("Index " + index + " out of bounds!");
+
+		// Storing the object
+		T removedObject = storage[index];
+
+		// Shifting everything to the left of it
+		for (int i = index; i <= size; i++)
+			storage[i] = storage[i+1];
+		size--;
+
+		// Halving size if the capacity ever reduces to a third
+		if (size == capacity()/3)
+			storage = Arrays.copyOf(storage, storage.length/2);
+
+		return removedObject;
+	}
+	
+	public Iterator<T> iterator() {
+		// Uses an anonymous class style, complete the iterator code
+		// below. Note that this uses the "diamond syntax" which is
+		// only available for nested classes from Java 9 forward.
+		// If you get an error on the next line you can add a <T>
+		// betwen the <> or you can (and should) update your 
+		// version of the JDK.
+		
+		return new Iterator<>() {
+			//instance variables here
+			//only _required_ methods are outlined below
+			//the interface also has some optional methods
+			//you may implement if you find them helpful
+			int currentPosition = 0;
+			public T next() {
+				//your code here
+				T currentObject = storage[currentPosition++];
+				return currentObject;
+			}
+			
+			public boolean hasNext() {
+				//your code here
+				return storage[currentPosition] != null;
+			}
+		};
+	}
+	
+	//******************************************************
+	//*******     BELOW THIS LINE IS TESTING CODE    *******
+	//*******      Edit it as much as you'd like!    *******
+	//******************************************************
+	
+	public String toString() {
+		//This method is provided for debugging purposes
+		//(use/modify as much as you'd like), it just prints
+		//out the list ifor easy viewing.
+		StringBuilder s = new StringBuilder("Dynamic array with " + size()
+			+ " items and a capacity of " + capacity() + ":");
+		for (int i = 0; i < size(); i++) {
+			s.append("\n  ["+i+"]: " + get(i));
+		}
+		return s.toString();
+		
+	}
+	
+	//JavaDoc note: How do you document a main? See Simulation.java for an example
+	public static void main(String args[]){
+		//These are _sample_ tests. If you're seeing all the "yays" that's
+		//an excellend first step! But it might not mean your code is 100%
+		//working... You may edit this as much as you want, so you can add
+		//own tests here, modify these tests, or whatever you need!
+		
+		DynamicArray<Integer> ida = new DynamicArray<>();
+		if ((ida.size() == 0) && (ida.capacity() == 2)){
+			System.out.println("Yay 1");
+		}
+
+		boolean ok = true;
+		for (int i=0; i<3;i++)
+			ok = ok && ida.add(i*5);
+		
+		if (ok && ida.size()==3 && ida.get(2) == 10 && ida.capacity() == 4 ){
+			System.out.println("Yay 2");
+		}
+		
+		ida.add(1,-10);
+		ida.add(4,100);
+		if (ida.set(1,-20)==-10 && ida.get(2) == 5 && ida.size() == 5 
+			&& ida.capacity() == 8 ){
+			System.out.println("Yay 3");
+		}
+		
+		if (ida.remove(0) == 0 && ida.remove(0) == -20 && ida.remove(2) == 100
+			&& ida.size() == 2 && ida.capacity() == 4 ){
+			System.out.println("Yay 4");
+		}
+		
+		//Uncomment this after doing the iterator for testing
+
+		System.out.print("Printing values: ");
+		for(Integer i : ida) {
+			System.out.print(i);
+			System.out.print(" ");
+		}
+
+	}
+}
---
+++ Simulation.java
@@ -0,0 +1,212 @@
+//TO DO: Add your name (as an author), complete the required methods.
+
+/**
+ *  The simulator. This tracks the elements in a grid
+ *  and coordinates that with the display.
+ *  
+ *  @author Dave Feinberg, K. Raven Russell, and [Your Name Here]
+ */
+public class Simulation {
+	
+	//******************************************************
+	//*******  DO NOT EDIT ANYTHING IN THIS SECTION  *******
+	//*******        But do read this section!       *******
+	//******************************************************
+	
+	/**
+	 *  The default number of rows the grid should have.
+	 */
+	public static final int INIT_ROWS = 120;
+	
+	/**
+	 *  The default number of columns the grid should have.
+	 */
+	public static final int INIT_COLS = 80;
+
+	/**
+	 *  The grid that represents the location of each element.
+	 */
+	private final DynamicArray<DynamicArray<Element>> grid;
+	
+	/**
+	 *  The GUI display.
+	 */
+	private final Display display;
+	
+	//******************************************************
+	//* END DO-NOT-EDIT SECTION -- DO NOT ADD MORE FIELDS! *
+	//******************************************************
+	
+	/**
+	 *  Sets up the instance variables (grid and display).
+	 *  
+	 *  @param withDisplay whether or not the display should be created (for testing purposes)
+	 */
+	public Simulation(boolean withDisplay) {
+		// Initialize the grid (see above) to the default size (see above).
+		// Fill the grid with empty void.
+		
+		// If the simulation should be created with a display, then set display
+		// (see above) to a new display. Use the title "Project 1 Simulation",
+		// the default number of rows and columns (see above), and the display
+		// constructor (see Display.java) to do this.
+		
+		// If the simulation should be created without a display, then initialize
+		// the display to null (or Java will yell at you).
+		grid = new DynamicArray<DynamicArray<Element>>(INIT_ROWS);
+		DynamicArray<Element> columns;
+
+		for(int i = 0; i < INIT_ROWS; i++) {
+			columns = new DynamicArray<Element>(INIT_COLS);
+			for(int j = 0; j < INIT_COLS; j++)
+				columns.add(new Empty());
+			grid.add(columns);
+		}
+		display = new Display("Project 1 Simulation", INIT_ROWS, INIT_COLS);
+	}
+
+	/**
+	 *  This is called when the user clicks on a location using the given tool.
+	 *  
+	 *  @param row the row where the action happened
+	 *  @param col the column where the actio happened
+	 *  @param newElem the element the user has created to put there
+	 */
+	public void locationClicked(int row, int col, Element newElem) {
+		// Put the new element in the grid at the row and column indicated.
+	}
+
+	/**
+	 *  Copies each element of grid's color into the display.
+	 */
+	public void updateDisplay() {
+		// Update each cell of the display (see above) with
+		// the correct color for that cell. The display has one
+		// cell per grid element (one-to-one) and the color of
+		// the cell is just the color of the element from the grid.
+		
+		// Remember: Display has a setColor(row, col, color) method,
+		// and elements have a getColor() method.
+	}
+	
+	/**
+	 *  Resizes the grid (if needed) to a bigger or smaller size.
+	 *  
+	 *  @param numRows the new number of rows the grid should have
+	 *  @param numCols the new number of columns the grid should have
+	 *  @return whether or not anything was changed
+	 */
+	public boolean resize(int numRows, int numCols) {
+		// This method might be called when there is no change... careful!
+		
+		// Parameter values are validated by display, but you
+		// should double check the numbers of rows/columns is
+		// at least 1 here. Ignore the resize request if an
+		// invalid value is given (don't crash the program...).
+		
+		// Add/remove all of _one_ column or row at a time until
+		//    the correct size is achieved. For example, if you
+		//    need to add 6 columns, DO NOT add six elements to
+		//    row 1, then 6 elements to row 2. Instead, add one
+		//    element to each row to complete the column, then
+		//    start on the next column.
+		// Multiple things might change! (Happens when dragging
+		//    the corners of the window...). In that case,
+		//    add/remove all rows before any columns
+		
+		// Rows are added to (and removed from) the top, while
+		// columns are added to (and removed from) on the right.
+		
+		// Added space is just filled with empty void.
+		
+		// Removed rows are just removed.
+		
+		// Removed columns work as follows:
+		//    If the removed element is as heavy (or heavier)
+		//        than the element to the left, it will try
+		//        to push that element to the left. If it
+		//        can push it, then it takes its place.
+		//        Remember: the push() method will move the
+		//        element if possible, don't reinvent the wheel!
+		//    If the removed element is lighter or can't
+		//        be pushed, the removed element is just lost.
+		
+		// It would be very, very wise to make yourself some private
+		// helper methods to do this part of the project.
+		return false;
+	}
+	
+	/**
+	 *  Indicates the private post where you have shown off your
+	 *  new element(s).
+	 *  
+	 *  @return the post where you describe your new element
+	 */
+	public static String newElementPost() {
+		//[GUI:Advanced] change this to return the FULL URL of
+		//the post where the grader can find your new element
+		//demo, but ONLY if you completed the [GUI:Advanced] part
+		//of the project.
+		return null;
+	}
+	
+	//******************************************************
+	//*******  DO NOT EDIT ANYTHING BELOW THIS LINE  *******
+	//*******        But do read this section!       *******
+	//******************************************************
+
+	/**
+	 *  Causes one random particle to maybe do something. Called
+	 *  repeatedly.
+	 */
+	public void step() {
+		int row = (int)(Math.random()*grid.size());
+		int col = (int)(Math.random()*grid.get(row).size()) ;
+		
+		grid.get(row).get(col).fall(grid, row, col);
+	}
+	
+	/**
+	 *  Game loop of the program (step, redraw, handlers, etc.).
+	 */
+	public void run() {
+		while (true) {
+			//step
+			for (int i = 0; i < display.getSpeed(); i++) step();
+			
+			//redraw everything
+			updateDisplay();
+			display.repaint();
+			
+			//wait for redrawing and for mouse
+			display.pause(1);
+			
+			//handle person actions (resize and tool usage)
+			if(display.handle(this) && display.pauseMode()) {
+				//for debugging
+				updateDisplay();
+				display.repaint();
+				display.pause(5000);
+			}
+		}
+	}
+	
+	/**
+	 *  Convenience method for GTA testing. Do not use this in
+	 *  your code... for testing purposes only.
+	 *  
+	 *  @return the private grid element
+	 */
+	public DynamicArray<DynamicArray<Element>> getGrid() {
+		return grid;
+	}
+	
+	/**
+	 *  Main method that kicks off the simulator.
+	 *  
+	 *  @param args not used
+	 */
+	public static void main(String[] args) {
+		(new Simulation(true)).run();
+	}
+}
